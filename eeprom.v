81a677ff8ba48e56197197a30740ac502a3c93d9
Mang Yau <m.yau@mail.utoronto.ca>
2017-03-28 14:55:09 -0400

    Fix eeprom compile errors.

--- eeprom.v
+++ eeprom.v
@@ -1,18 +1,28 @@
+/*
+  A simple eeprom module.
+ */ 
  module eeprom(
-    input [5:0] address,
+    input [2:0] address,
     input [1:0] data_in,
     input write_en,
-    input write,
     output reg[1:0] out
     );
     
-    reg [1:0] data [7:0];
+    // WORD_WIDTH is the number of bits we need to represent a color.
+    // ADDR_WIDTH is the number of addresses (levels).
+    parameter WORD_WIDTH = 2,
+              ADDR_WIDTH = 8;
     
-    always @(* or write) begin
+    reg [WORD_WIDTH -1 :0] data [ADDR_WIDTH - 1:0];
+    
+    wire [2:0] addr;
+    assign addr = address;
+    
+    always @(*) begin
         if (!write_en)
-            out <= data[address];
+            out = data[addr];
         else begin
-            data[address + 1, address] <= data_in;
+            data[addr] = data_in;
         end
     end
 endmodule
